1730d22b102d5da143c8a519ca9cdd571d677530
Sh1v4nk <shivankpandey113@gmail.com>
2024-10-05 22:46:09 +0530

    Replace json with env

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import discord 
+import discord
 import os
 import openai
 import difflib
-import json
+from dotenv import load_dotenv
 
-with open('config.json') as config_file:
-    config_data = json.load(config_file)
+load_dotenv()
 
-my_secret = config_data['PassKey']
-openai.api_key = config_data['OPENAI_API_KEY']
+my_secret = os.getenv("DISCORD_PASS_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 chat_history = []
 prev_response = ""
 
+
 # Define a class for the Discord bot
 class MyClient(discord.Client):
-
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f"Logged on as {self.user}!")
 
     async def on_message(self, message):
         global chat_history
@@ -35,18 +34,26 @@ class MyClient(discord.Client):
 
                 MAX_CHAT_HISTORY = 10
                 if len(chat_history) > MAX_CHAT_HISTORY:
-                    chat_history = chat_history[-MAX_CHAT_HISTORY:]  # Keep only the most recent messages
+                    chat_history = chat_history[
+                        -MAX_CHAT_HISTORY:
+                    ]  # Keep only the most recent messages
 
                 # Create a context by joining the chat history messages
                 context = "\n".join(chat_history)
 
                 # Generate a response using OpenAI API
                 response = None
-                while response is None or difflib.SequenceMatcher(None, response.choices[0].text, prev_response).ratio() > 0.7:
+                while (
+                    response is None
+                    or difflib.SequenceMatcher(
+                        None, response.choices[0].text, prev_response
+                    ).ratio()
+                    > 0.7
+                ):
                     # Build a prompt by combining the context, user input, and bot placeholder
                     prompt = f"Context:\n{context}\nUser: {content}\nChatterBox: "
                     response = openai.Completion.create(
-                        model="text-davinci-003", # Specify the model to use (GPT-3)
+                        model="text-davinci-003",  # Specify the model to use (GPT-3)
                         prompt=prompt,
                         temperature=0.7,
                         max_tokens=256,
@@ -59,6 +66,7 @@ class MyClient(discord.Client):
                 prev_response = message_to_send
 
                 await message.reply(message_to_send, mention_author=False)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
